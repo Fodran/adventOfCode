74b8c85d0502435b5f75017ca3d6df1967dbbe74
Fodran <Fodran>
2020-12-05 13:50:48 +0100

    Added day 5

---
+++ 2020/5/a/boarding.py
@@ -0,0 +1,53 @@
+def importData():
+    f = open("../input", "r")
+
+    data = []
+    for line in f:
+        data.append(line)
+
+    f.close()
+
+    return data
+
+
+
+def decodeSeat(code):
+    rows = range(128)
+    columns = range(8)
+    for letter in code:
+        if(letter == "F"):
+            rows = rows[:int(len(rows)/2)]
+        elif(letter == "B"):
+            rows = rows[int(len(rows)/2):]
+        elif(letter == "L"):
+            columns = columns[:int(len(columns)/2)]
+        else:
+            columns = columns[int(len(columns)/2):]
+    return [rows[0], columns[0]]
+
+
+
+def computeSeatId(coordinates):
+    return 8 * coordinates[0] + coordinates[1]
+
+
+
+def highestId(seats):
+    highestId = 0
+    for seat in seats:
+        seatId = computeSeatId(decodeSeat(seat))
+        if(seatId > highestId):
+            highestId = seatId
+    return highestId
+
+
+
+def main():
+    #print(highestId(["FBFBBFFRLR", "BFFFBBFRRR", "FFFBBBFRRR", "BBFFBBFRLL"]))
+    print(highestId(importData()))
+    return 0
+
+
+
+if __name__ == "__main__":
+    main()
